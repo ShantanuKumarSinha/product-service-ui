5bc1e0f9f90dedc41f6692a3d62edbe4e922c1cb
Shantanu Kumar <shan.raj93@gmail.com>
2024-08-29 21:16:29 +0530

    (refactor/observable) : replace deprecated method with the current method

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,24 +9,20 @@ import { Product } from '../models/product.model';
   imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
-  providers: []
+  providers: [],
 })
-export class ProductListComponent implements OnInit{
-
-  products : Product[] =[];
-  constructor(private productService : ProductService){}
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts(): void{
-    this.productService.getAllProducts().subscribe((response)=>{
-      this.products = response;
-    },
-  (error)=>{
-    console.error('Error fetching response',error);
-  })
+  getProducts(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (response) => (this.products = response),
+      error: (error) => console.error(error);
+    });
   }
-
 }
