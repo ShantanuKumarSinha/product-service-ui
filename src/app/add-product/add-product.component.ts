c73b7e1ea0b935d26d580dfed44c43e6cf7b334a
Shantanu Kumar <53042260+ShantanuKumarSinha@users.noreply.github.com>
2024-09-05 19:16:57 +0530

    Feature/add product (#9)
    * (feature/add-product): add-product component changes
    
    * (feature/add-product): linting changes

---
+++ src/app/add-product/add-product.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'app-add-product',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './add-product.component.html',
+  styleUrl: './add-product.component.scss',
+})
+export class AddProductComponent {
+  addProductForm: FormGroup;
+  isEditing = true;
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService
+  ) {
+    this.addProductForm = this.formBuilder.group({
+      productId: [{ value: 0, disabled: true }],
+      productName: [{ value: '', disabled: true }, Validators.required],
+      price: [
+        { value: 0, disable: true },
+        [Validators.required, Validators.min(0)],
+      ],
+      brand: [{ value: '', disabled: true }, Validators.required],
+      quantity: [
+        { value: 0, disable: true },
+        [Validators.required, Validators.min(1)],
+      ],
+    });
+  }
+
+  onSubmit() {
+    if (this.addProductForm.valid) {
+      const product: Product = this.mapFormToModel();
+      this.productService.createProduct(product).subscribe({
+        next: (response) => console.log(response),
+        error: (err) => console.log(err),
+        complete: () => console.log('Created'),
+      });
+    }
+  }
+
+  mapFormToModel(): Product {
+    const formValues = this.addProductForm.value;
+    const product: Product = {
+      productName: formValues.productName,
+      price: formValues.price,
+      brand: formValues.brand,
+      quantity: formValues.quantity,
+    };
+    return product;
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [ProductListComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
