362d78c8799e71440a6e885f5f9492240737b3e7
Shantanu Kumar <shan.raj93@gmail.com>
2024-09-05 19:13:05 +0530

    (feature/add-product): add-product component changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
+  { path: 'products/add', component: AddProductComponent },
   { path: 'products/:productId', component: ProductDetailsComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: '**', redirectTo: 'products' }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent implements OnInit {
+@Input() productForm: FormGroup;
+@Input() isEditing!: boolean;
+@Output() formSubmit = new EventEmitter<void>();
+
+constructor(private formBuilder : FormBuilder) {
+  this.productForm = this.formBuilder.group({
+    productId: [{ value: 0, disabled: true }],
+    productName: [{ value: '', disabled: true }, Validators.required],
+    price: [
+      { value: 0, disable: true },
+      [Validators.required, Validators.min(0)],
+    ],
+    brand: [{ value: '', disabled: true }, Validators.required],
+    quantity: [
+      { value: 0, disable: true },
+      [Validators.required, Validators.min(1)],
+    ],
+  });
+}
+  ngOnInit(): void {
+    if(this.isEditing)
+      this.enableFormControls();
+    else 
+    this.disableFormControls();
+  }
+
+  enableFormControls(){
+    this.productForm.get('productName')?.enable();
+    this.productForm.get('price')?.enable();
+    this.productForm.get('quantity')?.enable();
+    this.productForm.get('brand')?.enable();
+  }
+
+  disableFormControls(){
+    this.productForm.get('productName')?.disable();
+    this.productForm.get('price')?.disable();
+    this.productForm.get('quantity')?.disable();
+    this.productForm.get('brand')?.disable();
+  }
+
+
+onSubmit() {
+  if (this.productForm.valid) {
+    this.formSubmit.emit();
+  }
+}
+ 
+}
